8dcbfba01dc65bc313a426343122d004708e2903
Dinesh <1dineshc0@gmail.com>
2022-06-05 12:55:31 +0530

    Added work list

--- app.js
+++ app.js
@@ -1,40 +1,55 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const app = express();
-const tasks =[];
+const basicTasks =[];
+const workTasks =[];
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.get("/",function(req,res){
 
-  var today = new Date();
-  var options ={
-    weekday : "long",
-    day :"numeric",
-    month:"long"
-  };
-
-  var day = today.toLocaleDateString("en-US",options);
-
+  let head = date.getDate();
   res.render("list",{
-    kindOfDay : day,
-    newtasks : tasks
+    heading : head,
+    newtasks : basicTasks,
+    typeList: "basic"
   });
 
+});
+
+app.get("/work",function(req,res){
+
+  let head = "Work";
+  res.render("list",{
+    heading : head,
+    newtasks : workTasks,
+    typeList: "working"
+  });
 
 });
 
 app.post("/",function(req,res){
+console.log(req.body);
+  var task = req.body.task;
+  if(req.body.typeTask == "basic"){
+    basicTasks.push(task);
+    res.redirect("/");
+  }else if(req.body.typeTask == "working"){
+    workTasks.push(task);
+    res.redirect("/work");
+  }
 
-  var task =req.body.task;
-  tasks.push(task);
-  res.redirect("/");
+
 
 });
+
+
+
 
 app.listen(3000,function(){
   console.log("server started");
